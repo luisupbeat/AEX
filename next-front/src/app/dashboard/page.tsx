9403cb7de1b7a12442dbe8dd8abf25035a14ecd7
Luis <ronaluis27@gmail.com>
2024-07-23 22:23:18 -0500

    error tailwind

--- next-front/src/app/dashboard/page.tsx
+++ next-front/src/app/dashboard/page.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 export default function Page() {
   return (
     <div>Hello, dashboard is here
-        <div className="suspende">
-        </div>
+      <div className="suspende">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <h1>Hello world!</h1>
+      </div>
     </div>
   )
 }
--- next-front/src/app/page.tsx
+++ next-front/src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
- 
+
 export const metadata: Metadata = {
   title: 'Next.js',
 }
- 
+
 export default function Page() {
-  return <Link href="/dashboard">Dashboard</Link>
+  return (
+    <main>
+
+      <Link href="/dashboard">Dashboard</Link>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1>Hello world!</h1>
+    </main>
+  )
 }
